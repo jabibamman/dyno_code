e7c13df6d23017803febc0a12f414d96e88e51a4
James ABIB <james.bidule@gmail.com>
2024-05-26 17:07:42 +0200

    refacto: Refactored executor script
    The executor script has been significantly refactored. The changes include the addition of new functions to handle code execution and error handling more efficiently. This includes a generic function for executing code in different languages, as well as a specific function for compiling and executing Rust code. The case statement has also been updated to use these new functions.

--- executor_script.sh
+++ executor_script.sh
@@ -1,21 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
 LANGUAGE=$1
 CODE=$2
 
+execute_code() {
+  cmd=$1
+  code=$2
+
+  RESULT=$(echo "$code" | $cmd 2>&1)
+  EXIT_CODE=$?
+  echo "$RESULT"
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "EXECUTOR_ERROR"
+    exit 1
+  fi
+}
+
+compile_and_execute_rust() {
+  code=$1
+
+  echo "$code" > temp.rs
+  COMPILE_RESULT=$(rustc temp.rs 2>&1)
+  COMPILE_EXIT_CODE=$?
+  if [ $COMPILE_EXIT_CODE -ne 0 ]; then
+    echo "$COMPILE_RESULT"
+    rm temp.rs
+    echo "EXECUTOR_ERROR"
+    exit 1
+  fi
+  EXEC_RESULT=$(./temp 2>&1)
+  EXEC_EXIT_CODE=$?
+  echo "$EXEC_RESULT"
+  rm temp.rs temp
+  if [ $EXEC_EXIT_CODE -ne 0 ]; then
+    echo "EXECUTOR_ERROR"
+    exit 1
+  fi
+}
+
 case $LANGUAGE in
   "python")
-    echo "$CODE" | python3
+    execute_code "python3" "$CODE"
     ;;
   "lua")
-    echo "$CODE" | lua
+    execute_code "lua" "$CODE"
     ;;
   "rust")
-    echo "$CODE" > temp.rs
-    rustc temp.rs && ./temp
+    compile_and_execute_rust "$CODE"
     ;;
   *)
-    echo "Unsupported language: $LANGUAGE"
+    echo "Unsupported language EXECUTOR_ERROR"
     exit 1
     ;;
 esac
